fad6a3e027e34671df1fc170a590f2ebbf3d82ef
slashadmin <christoph@engelmayer.me>
2017-11-19 17:55:19 +0100

    VIM: Switched to material theme

--- vim/install.sh
+++ vim/install.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# Install Material color theme
-if [ ! -f $HOME/.vim/colors/material-theme.vim ]; then
-  curl -o $HOME/.vim/colors/material-theme.vim https://github.com/jdkanani/vim-material-theme/raw/master/colors/material-theme.vim -L
-fi
-
 # Install Vundle and plugins
 if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
 	git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim && \
