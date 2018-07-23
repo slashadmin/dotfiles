f921b6c0a77f39032a8d79a9c5dd13da5fcf2c34
christophengelmayer <christoph.engelmayer@asoluto.com>
2018-07-23 10:39:51 +0200

    Renamed osx to macos

---
+++ macos/set-defaults.sh
@@ -0,0 +1,5 @@
+# Sets macOS defaults.
+# Run ./set-defaults.sh and you'll be good to go.
+
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
