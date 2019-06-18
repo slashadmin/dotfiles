54dd09f8630738f0b2bf16235b5053931c6f3028
Christoph Engelmayer <christoph.engelmayer@asoluto.com>
2019-06-18 12:03:10 +0200

    Neos: Remove temporary files when flushing cache

--- neos/aliases.zsh
+++ neos/aliases.zsh
@@ -1,4 +1,4 @@
 alias f='./flow'
 alias fsr='./flow server:run'
 alias ff='./flow flow:cache:flush'
-alias fff='./flow flow:cache:flush --force'
+alias fff='./flow flow:cache:flush --force && rm -rf Data/Temporary'
